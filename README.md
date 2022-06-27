a3485555bbd1848441ee0c9848e034c7e0a8fcd1
Ricardo <sricardoaguiars@gmail.com>
2022-06-27 00:38:47 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Desafio de Projeto sobre Git/Github da DIO.
 
 Neste projeto vou fazer uma breve apresentação do Git e Github e alguns comandos básicos.
+
+Links utilizados:
+https://blog.geekhunter.com.br/comandos-git-mais-utilizados/#Porque_preciso_dos_comandos_Git
+https://kenzie.com.br/blog/o-que-e-git/
+https://www.techtudo.com.br/listas/2021/05/o-que-e-o-github-veja-para-que-serve-a-rede-social-de-programadores.ghtml
---
+++ main.js
@@ -0,0 +1,20 @@
+/* ABRE E FECHA O MENU QUANDO CLICAR NO ICONE: HAMBURGUER E X */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* QUANDO CLICAR EM UM ITEM DO MENU, ESCONDER O MENU */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
